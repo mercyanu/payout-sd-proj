a3dd22d1f26f6823d344229cc1d84b4412d43491
Anuoluwapo Akintoye <mercyanu14@gmail.com>
2022-12-04 22:53:10 +0000

    UI done, start logic

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        cust_redError: 'hsl(0, 100%, 66%)',
+        cust_lightGrayishViolet: 'hsl(270, 3%, 87%)',
+        cust_DarkGrayishViolet: 'hsl(279, 6%, 55%)',
+        cust_darkViolet: 'hsl(278, 68%, 11%)'
+      },
+      backgroundImage:{
+        'hero': "url('/images/bg-main-desktop.png')";
+      }
+    },
   },
   plugins: [],
 }
